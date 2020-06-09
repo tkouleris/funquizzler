7433509ea353d0566235fff38aa15f7850b56324
darkheart101 <kurokokoro@windowslive.com>
2020-06-09 21:23:06 +0300

    initial commit

---
+++ src/main/java/com/tkouleris/funquizzler/FunquizzlerApplication.java
@@ -0,0 +1,13 @@
+package com.tkouleris.funquizzler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunquizzlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunquizzlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tkouleris/funquizzler/FunquizzlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkouleris.funquizzler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunquizzlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
