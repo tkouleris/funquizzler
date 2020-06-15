724af3a751b13a53d3cde44687dda258ae77fb3a
darkheart101 <kurokokoro@windowslive.com>
2020-06-15 21:02:06 +0300

    bug fix: title validation

--- src/main/java/com/tkouleris/funquizzler/service/QuizService.java
+++ src/main/java/com/tkouleris/funquizzler/service/QuizService.java
@@ -12,19 +12,17 @@ public class QuizService {
 	private QuizRepository R_quiz;
 	
 	public Quiz create(Quiz newQuiz)
-	{		
-		if( !titleIsValid(newQuiz.getTitle()) )
+	{
+		if( titleIsValid(newQuiz.getTitle()) == false)
 		{
 			throw new IllegalArgumentException("Your quiz must have a title");
-		}
-				
-		
+		}						
 		R_quiz.save(newQuiz);		
 		return newQuiz;
 	}
 	
 	private Boolean titleIsValid(String quizTitle)
 	{
-		return quizTitle == null || quizTitle.isEmpty() || quizTitle.trim().isEmpty();
+		return quizTitle != null && !quizTitle.isEmpty() && !quizTitle.trim().isEmpty();
 	}
 }
