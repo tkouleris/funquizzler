3c672dac158596e91676bcb2a2fc75f7def5490e
darkheart101 <kurokokoro@windowslive.com>
2020-06-18 22:06:08 +0300

    feature: Quiz Service delete

--- src/main/java/com/tkouleris/funquizzler/service/QuizService.java
+++ src/main/java/com/tkouleris/funquizzler/service/QuizService.java
@@ -8,9 +8,10 @@ import com.tkouleris.funquizzler.model.Quiz;
 
 @Service
 public class QuizService {
+	
 	@Autowired
 	private QuizRepository R_quiz;
-	
+
 	public Quiz create(Quiz newQuiz)
 	{
 		if( titleIsValid(newQuiz.getTitle()) == false)
@@ -20,7 +21,24 @@ public class QuizService {
 		R_quiz.save(newQuiz);		
 		return newQuiz;
 	}
-	
+
+	public Quiz update(Quiz updatedQuiz)
+	{
+		long quiz_id = updatedQuiz.getId();
+		Quiz currentQuiz = R_quiz.findById(quiz_id).orElse(null);
+		currentQuiz.setTitle(updatedQuiz.getTitle());
+		currentQuiz.setPublished_at(updatedQuiz.getPublished_at());
+		R_quiz.save(currentQuiz);
+		
+		return currentQuiz;
+	}
+
+	public void delete(long quiz_id)
+	{
+		Quiz toDeleteQuiz = R_quiz.findById(quiz_id).orElse(null);
+		R_quiz.delete(toDeleteQuiz);
+	}
+
 	private Boolean titleIsValid(String quizTitle)
 	{
 		return quizTitle != null && !quizTitle.isEmpty() && !quizTitle.trim().isEmpty();
